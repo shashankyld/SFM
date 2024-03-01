805b5da021c3da9439481866a33426d521e8c0e8
shashankyld <shashankyld@gmail.com>
2024-03-01 18:31:58 +0100

    Only feature extraction, doesnt include tracking over frames

---
+++ my_superpoint.py
@@ -0,0 +1,381 @@
+# This is a re-implementation of the SuperPoint
+# Code adapted from magicLeap's implementation of SuperPoint
+# Will use their pre-trained weights with my datasets. 
+
+import argparse
+import glob
+import numpy as np
+import os
+import time
+import cv2
+import torch
+
+print("OpenCV version: ", cv2.__version__)
+print("PyTorch version: ", torch.__version__)
+
+# Jet colormap for visualization : Same values as used by Magicleap
+myjet = np.array([[0.        , 0.        , 0.5       ],
+                  [0.        , 0.        , 0.99910873],
+                  [0.        , 0.37843137, 1.        ],
+                  [0.        , 0.83333333, 1.        ],
+                  [0.30044276, 1.        , 0.66729918],
+                  [0.66729918, 1.        , 0.30044276],
+                  [1.        , 0.90123457, 0.        ],
+                  [1.        , 0.48002905, 0.        ],
+                  [0.99910873, 0.07334786, 0.        ],
+                  [0.5       , 0.        , 0.        ]])
+
+# SuperPoint
+
+class SuperPointNet(torch.nn.Module):
+    """ This implementation is copied from magicLeap's implementation of SuperPoint (Pytorch) """
+    def __init__(self):
+        super(SuperPointNet, self).__init__()
+        self.relu = torch.nn.ReLU(inplace=True)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        c1, c2, c3, c4, c5, d1 = 64, 64, 128, 128, 256, 256
+        # Shared Encoder.
+        self.conv1a = torch.nn.Conv2d(1, c1, kernel_size=3, stride=1, padding=1)
+        self.conv1b = torch.nn.Conv2d(c1, c1, kernel_size=3, stride=1, padding=1)
+        self.conv2a = torch.nn.Conv2d(c1, c2, kernel_size=3, stride=1, padding=1)
+        self.conv2b = torch.nn.Conv2d(c2, c2, kernel_size=3, stride=1, padding=1)
+        self.conv3a = torch.nn.Conv2d(c2, c3, kernel_size=3, stride=1, padding=1)
+        self.conv3b = torch.nn.Conv2d(c3, c3, kernel_size=3, stride=1, padding=1)
+        self.conv4a = torch.nn.Conv2d(c3, c4, kernel_size=3, stride=1, padding=1)
+        self.conv4b = torch.nn.Conv2d(c4, c4, kernel_size=3, stride=1, padding=1)
+        # Detector Head.
+        self.convPa = torch.nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
+        self.convPb = torch.nn.Conv2d(c5, 65, kernel_size=1, stride=1, padding=0)
+        # Descriptor Head.
+        self.convDa = torch.nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
+        self.convDb = torch.nn.Conv2d(c5, d1, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        """ Forward pass that jointly computes unprocessed point and descriptor
+        tensors.
+        Input
+        x: Image pytorch tensor shaped N x 1 x H x W.
+        Output
+        semi: Output point pytorch tensor shaped N x 65 x H/8 x W/8.
+        desc: Output descriptor pytorch tensor shaped N x 256 x H/8 x W/8.
+        """
+        # Shared Encoder.
+        x = self.relu(self.conv1a(x))
+        x = self.relu(self.conv1b(x))
+        x = self.pool(x)
+        x = self.relu(self.conv2a(x))
+        x = self.relu(self.conv2b(x))
+        x = self.pool(x)
+        x = self.relu(self.conv3a(x))
+        x = self.relu(self.conv3b(x))
+        x = self.pool(x)
+        x = self.relu(self.conv4a(x))
+        x = self.relu(self.conv4b(x))
+        # Detector Head.
+        cPa = self.relu(self.convPa(x))
+        semi = self.convPb(cPa)
+        # Descriptor Head.
+        cDa = self.relu(self.convDa(x))
+        desc = self.convDb(cDa)
+        dn = torch.norm(desc, p=2, dim=1) # Compute the norm.
+        desc = desc.div(torch.unsqueeze(dn, 1)) # Divide by norm to normalize.
+        return semi, desc
+
+
+# SuperPoint FrontEnd
+    
+class SuperPointFrontend(object):
+  """ Wrapper around pytorch net to help 
+      with pre and post image processing. 
+      Useful for preprocessing steps like
+      Non_Maxima_Suppresion of interest points
+      Note: This part of code is copied from MagicLeap's implementation    
+  """
+  def __init__(self, weights_path, nms_dist, conf_thresh, nn_thresh,
+               cuda=False):
+    self.name = 'SuperPoint'
+    self.cuda = cuda
+    self.nms_dist = nms_dist
+    self.conf_thresh = conf_thresh
+    self.nn_thresh = nn_thresh # L2 descriptor distance for good match.
+    self.cell = 8 # Size of each output cell. Keep this fixed.
+    self.border_remove = 4 # Remove points this close to the border.
+
+    # Load the network in inference mode.
+    self.net = SuperPointNet()
+    if cuda:
+      # Train on GPU, deploy on GPU.
+      self.net.load_state_dict(torch.load(weights_path))
+      self.net = self.net.cuda()
+    else:
+      # Train on GPU, deploy on CPU.
+      self.net.load_state_dict(torch.load(weights_path,
+                               map_location=lambda storage, loc: storage))
+    self.net.eval()
+
+  def nms_fast(self, in_corners, H, W, dist_thresh):
+    """
+    Run a faster approximate Non-Max-Suppression on numpy corners shaped:
+      3xN [x_i,y_i,conf_i]^T
+  
+    Algo summary: Create a grid sized HxW. Assign each corner location a 1, rest
+    are zeros. Iterate through all the 1's and convert them either to -1 or 0.
+    Suppress points by setting nearby values to 0.
+  
+    Grid Value Legend:
+    -1 : Kept.
+     0 : Empty or suppressed.
+     1 : To be processed (converted to either kept or supressed).
+  
+    NOTE: The NMS first rounds points to integers, so NMS distance might not
+    be exactly dist_thresh. It also assumes points are within image boundaries.
+  
+    Inputs
+      in_corners - 3xN numpy array with corners [x_i, y_i, confidence_i]^T.
+      H - Image height.
+      W - Image width.
+      dist_thresh - Distance to suppress, measured as an infinty norm distance.
+    Returns
+      nmsed_corners - 3xN numpy matrix with surviving corners.
+      nmsed_inds - N length numpy vector with surviving corner indices.
+    """
+    grid = np.zeros((H, W)).astype(int) # Track NMS data.
+    inds = np.zeros((H, W)).astype(int) # Store indices of points.
+    # Sort by confidence and round to nearest int.
+    inds1 = np.argsort(-in_corners[2,:])
+    corners = in_corners[:,inds1]
+    rcorners = corners[:2,:].round().astype(int) # Rounded corners.
+    # Check for edge case of 0 or 1 corners.
+    if rcorners.shape[1] == 0:
+      return np.zeros((3,0)).astype(int), np.zeros(0).astype(int)
+    if rcorners.shape[1] == 1:
+      out = np.vstack((rcorners, in_corners[2])).reshape(3,1)
+      return out, np.zeros((1)).astype(int)
+    # Initialize the grid.
+    for i, rc in enumerate(rcorners.T):
+      grid[rcorners[1,i], rcorners[0,i]] = 1
+      inds[rcorners[1,i], rcorners[0,i]] = i
+    # Pad the border of the grid, so that we can NMS points near the border.
+    pad = dist_thresh
+    grid = np.pad(grid, ((pad,pad), (pad,pad)), mode='constant')
+    # Iterate through points, highest to lowest conf, suppress neighborhood.
+    count = 0
+    for i, rc in enumerate(rcorners.T):
+      # Account for top and left padding.
+      pt = (rc[0]+pad, rc[1]+pad)
+      if grid[pt[1], pt[0]] == 1: # If not yet suppressed.
+        grid[pt[1]-pad:pt[1]+pad+1, pt[0]-pad:pt[0]+pad+1] = 0
+        grid[pt[1], pt[0]] = -1
+        count += 1
+    # Get all surviving -1's and return sorted array of remaining corners.
+    keepy, keepx = np.where(grid==-1)
+    keepy, keepx = keepy - pad, keepx - pad
+    inds_keep = inds[keepy, keepx]
+    out = corners[:, inds_keep]
+    values = out[-1, :]
+    inds2 = np.argsort(-values)
+    out = out[:, inds2]
+    out_inds = inds1[inds_keep[inds2]]
+    return out, out_inds
+
+  def run(self, img):
+    """ Process a numpy image to extract points and descriptors.
+    Input
+      img - HxW numpy float32 input image in range [0,1].
+    Output
+      corners - 3xN numpy array with corners [x_i, y_i, confidence_i]^T.
+      desc - 256xN numpy array of corresponding unit normalized descriptors.
+      heatmap - HxW numpy heatmap in range [0,1] of point confidences.
+      """
+    assert img.ndim == 2, 'Image must be grayscale.'
+    assert img.dtype == np.float32, 'Image must be float32.'
+    H, W = img.shape[0], img.shape[1]
+    inp = img.copy()
+    inp = (inp.reshape(1, H, W))
+    inp = torch.from_numpy(inp)
+    inp = torch.autograd.Variable(inp).view(1, 1, H, W)
+    if self.cuda:
+      inp = inp.cuda()
+    # Forward pass of network.
+    outs = self.net.forward(inp)
+    semi, coarse_desc = outs[0], outs[1]
+    # Convert pytorch -> numpy.
+    semi = semi.data.cpu().numpy().squeeze()
+    # --- Process points.
+    dense = np.exp(semi) # Softmax.
+    dense = dense / (np.sum(dense, axis=0)+.00001) # Should sum to 1.
+    # Remove dustbin.
+    nodust = dense[:-1, :, :]
+    # Reshape to get full resolution heatmap.
+    Hc = int(H / self.cell)
+    Wc = int(W / self.cell)
+    nodust = nodust.transpose(1, 2, 0)
+    heatmap = np.reshape(nodust, [Hc, Wc, self.cell, self.cell])
+    heatmap = np.transpose(heatmap, [0, 2, 1, 3])
+    heatmap = np.reshape(heatmap, [Hc*self.cell, Wc*self.cell])
+    xs, ys = np.where(heatmap >= self.conf_thresh) # Confidence threshold.
+    if len(xs) == 0:
+      return np.zeros((3, 0)), None, None
+    pts = np.zeros((3, len(xs))) # Populate point data sized 3xN.
+    pts[0, :] = ys
+    pts[1, :] = xs
+    pts[2, :] = heatmap[xs, ys]
+    pts, _ = self.nms_fast(pts, H, W, dist_thresh=self.nms_dist) # Apply NMS.
+    inds = np.argsort(pts[2,:])
+    pts = pts[:,inds[::-1]] # Sort by confidence.
+    # Remove points along border.
+    bord = self.border_remove
+    toremoveW = np.logical_or(pts[0, :] < bord, pts[0, :] >= (W-bord))
+    toremoveH = np.logical_or(pts[1, :] < bord, pts[1, :] >= (H-bord))
+    toremove = np.logical_or(toremoveW, toremoveH)
+    pts = pts[:, ~toremove]
+    # --- Process descriptor.
+    D = coarse_desc.shape[1]
+    if pts.shape[1] == 0:
+      desc = np.zeros((D, 0))
+    else:
+      # Interpolate into descriptor map using 2D point locations.
+      samp_pts = torch.from_numpy(pts[:2, :].copy())
+      samp_pts[0, :] = (samp_pts[0, :] / (float(W)/2.)) - 1.
+      samp_pts[1, :] = (samp_pts[1, :] / (float(H)/2.)) - 1.
+      samp_pts = samp_pts.transpose(0, 1).contiguous()
+      samp_pts = samp_pts.view(1, 1, -1, 2)
+      samp_pts = samp_pts.float()
+      if self.cuda:
+        samp_pts = samp_pts.cuda()
+      desc = torch.nn.functional.grid_sample(coarse_desc, samp_pts)
+      desc = desc.data.cpu().numpy().reshape(D, -1)
+      desc /= np.linalg.norm(desc, axis=0)[np.newaxis, :]
+    return pts, desc, heatmap
+
+# Images DataLoader
+    
+class DataLoader():
+    def __init__(self, data_path, H = 1080, W = 1920):
+      self.dir_path = data_path
+      # Find out the extension of the datapath
+      file_ext = glob.glob(data_path + "/*")[0].split(".")[-1]
+      print("File Extension:", file_ext)
+      self.images = glob.glob(data_path + "/*." + file_ext)
+      self.images.sort()
+    #   print("Images: ", self.images) # (Only for debugging)
+      self.H = H
+      self.W = W
+
+    def __len__(self):
+       return len(self.images)
+    
+    def __getitem__(self, index):
+      img_path = self.images[index]
+      img = cv2.imread(img_path)
+      img = cv2.resize(img, (self.W, self.H))
+      img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # SuperPoint Expects a GrayImage
+      img = img.astype('float32') / 255.
+      return img
+      
+       
+
+
+if __name__ == '__main__':
+
+    # Parsing command line arguments
+    parser = argparse.ArgumentParser(description = "This script runs SuperPoint on my data")
+    
+    # Adding specific args
+    parser.add_argument('dataset_path', type = str, default='', 
+                        help = "Path to the data directory")
+    parser.add_argument('weights_path', type = str, default = '',
+                        help = "Path to the pretrained SuperPointNet weights")
+    parser.add_argument('--H', type=int, default = 600,
+                        help='Input image height (default used by magicleap = 120)')
+    parser.add_argument('--W', type = int, default = 800,
+                        help="Input image width (default used by magicleap = 160)")
+    parser.add_argument('--nms_dist', type=int, default=4,
+                        help='Non Maximum Suppression (NMS) distance (default: 4).')
+    parser.add_argument('--conf_thresh', type=float, default=0.015,
+                        help='Detector confidence threshold (default: 0.015).')
+    parser.add_argument('--nn_thresh', type=float, default=0.7,
+                        help='Descriptor matching threshold (default: 0.7).')
+    parser.add_argument('--cuda', action='store_true',
+                        help='Use cuda GPU to speed up network processing speed (default: False)')
+    parser.add_argument('--display_scale', type=int, default=1,
+                        help='Factor to scale output visualization (default: 2).')
+    parser.add_argument('--no_display', action='store_true',
+                        help='Do not display images to screen. Useful if running remotely (default: False).')
+    parser.add_argument('--waitkey', type=int, default=1,
+                        help='OpenCV waitkey time in ms (default: 1).')
+    args = parser.parse_args()
+    print("Args: ", args)
+
+    # Font parameters for visualizaton : Same values as used by MagicLeap
+    font = cv2.FONT_HERSHEY_DUPLEX
+    font_clr = (255, 255, 255)
+    font_pt = (4, 12)
+    font_sc = 0.4
+
+      # Create a window to display the demo.
+    if not args.no_display:
+        win = 'SuperPoint Tracker'
+        cv2.namedWindow(win)
+    else:
+        print('Skipping visualization, will not show a GUI.')
+
+    # Running the SuperPoint on the whole dataset
+    sp_frontend = SuperPointFrontend(weights_path=args.weights_path,
+                                     nms_dist = args.nms_dist,
+                                     conf_thresh = args.conf_thresh,
+                                     nn_thresh = args.nn_thresh,
+                                     cuda = args.cuda)
+    print("Loaded the frontend of SuperPoint")
+    data_path = args.dataset_path
+    data_loader = DataLoader(args.dataset_path, H = args.H, W = args.W)
+    frame_id = 0
+    while True:
+      start1 = time.time()
+      current_frame = data_loader[frame_id]
+
+      # Checking Original Size of the images in the dataset
+      print("Original Images Size: ", current_frame.shape)
+      print("Currently process frame: ", frame_id)
+
+      pts, desc, heatmap = sp_frontend.run(current_frame)
+      
+      # Showing current interest points
+      out1 = (np.dstack((current_frame, current_frame, current_frame)) * 255.).astype('uint8')
+      for pt in pts.T:
+        pt1 = (int(round(pt[0])), int(round(pt[1])))
+        cv2.circle(out1, pt1, 1, (0, 255, 0), -1, lineType=16)
+      cv2.putText(out1, 'Raw Point Detections', font_pt, font, font_sc, font_clr, lineType=16)
+      
+      # Extra output -- Show the point confidence heatmap.
+      if heatmap is not None:
+        min_conf = 0.001
+        heatmap[heatmap < min_conf] = min_conf
+        heatmap = -np.log(heatmap)
+        heatmap = (heatmap - heatmap.min()) / (heatmap.max() - heatmap.min() + .00001)
+        out2 = myjet[np.round(np.clip(heatmap*10, 0, 9)).astype('int'), :]
+        out2 = (out2*255).astype('uint8')
+      else:
+        out2 = np.zeros_like(out1)
+        cv2.putText(out2, 'Raw Point Confidences', font_pt, font, font_sc, font_clr, lineType=16)
+    
+      out = np.hstack((out1, out2)) 
+   
+      out = cv2.resize(out, (3*args.display_scale*args.W, args.display_scale*args.H))
+
+      # Display visualization image to screen.
+      if not args.no_display:
+        cv2.imshow(win, out)
+        key = cv2.waitKey(args.waitkey) & 0xFF
+        if key == ord('q'):
+            print('Quitting, \'q\' pressed.')
+            break
+
+
+      frame_id += 1
+      
+      if frame_id == len(data_loader):
+        break 
+      
+    cv2.destroyAllWindows()
+    print("Rano out of frames")
